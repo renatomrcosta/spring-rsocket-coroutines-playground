617d82fa5c674b57f34aa85e08ad4517e20c91da
Renato Costa <renato.costa@jimdo.com>
2020-09-26 23:56:14 +0200

    broke app down into kotlin and java modules

---
+++ spring-rsocket-kotlin-module/src/test/kotlin/com/xunfos/springrsocketkotlinmodule/SpringRsocketKotlinModuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xunfos.springrsocketkotlinmodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRsocketKotlinModuleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
