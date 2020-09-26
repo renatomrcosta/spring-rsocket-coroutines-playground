aff9a85356de67c0fd6cf0e1a29052d5a7676d1b
Renato Costa <renato.rezende.costa@gmail.com>
2020-09-26 21:28:42 +0200

    initial commit

---
+++ src/test/kotlin/com/xunfos/springrsocketcoroutinesplayground/SpringRsocketCoroutinesPlaygroundApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.xunfos.springrsocketcoroutinesplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRsocketCoroutinesPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
